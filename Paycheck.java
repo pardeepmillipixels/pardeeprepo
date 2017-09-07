5d332ba3df37662139ed11e5339c5af57b8d9cf7
pardeepmillipixels <31564222+pardeepmillipixels@users.noreply.github.com>
2017-09-07 16:23:56 +0530

    Create Paycheck.java

---
+++ Paycheck.java
@@ -0,0 +1,9 @@
+public class Paycheck {
+    public static void main(String args[]) {
+        double amountInAccount;
+        amountInAccount = 128.57;
+        System.out.print("You earned $");
+        System.out.print(amountInAccount);
+        System.out.println(" at work today.");
+    }
+}
